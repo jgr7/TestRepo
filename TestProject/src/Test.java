ca08751f7278c43259341e86dcd44f0881392f26
jgrazka <jaroslaw.grazka@altkom.pl>
2016-04-05 23:06:46 +0200

    qwewq

--- TestProject/src/Test.java
+++ TestProject/src/Test.java
@@ -1,11 +1,1 @@
-
-public class Test {
-<<<<<<< HEAD
-//qwwqqwwdfsdfdsfdsfd1212121212123123123123123123
-=======
-//qwwqqwwdfsdfdsfdsfd1212121212qqqqqqqqqqqqqqqqqq
->>>>>>> branch 'master' of https://github.com/jgr7/TestRepo.git
-=======
-public class Test qwqwqw{
-//qwwqqww
-}
+123123123
